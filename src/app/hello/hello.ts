e70f09f4b07573ca931bb2c7878484cc16ce5e39
Leonardo Queiroz Rocha <47957548+LeonardoQueirozRocha@users.noreply.github.com>
2026-06-01 20:32:43 -0300

    wip: Introducing Signals

--- src/app/hello/hello.ts
+++ src/app/hello/hello.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -7,13 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './hello.scss',
 })
 export class Hello {
-
   protected title = 'Welcome to Modern Angular!';
-  
+
   protected isDisabled = false;
 
   protected onClick() {
     console.log('Button clicked!');
     this.isDisabled = !this.isDisabled;
+  }
+
+  protected count = signal(0);
+
+  protected increaseCounter(): void {
+    this.count.update((value) => value + 1);
+  }
+
+  protected decreaseCounter(): void {
+    this.count.update((value) => value - 1);
+  }
+
+  protected restCounter(): void {
+    this.count.set(0);
   }
 }
